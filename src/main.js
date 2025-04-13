3011a7ece2d57d189eef77e2033e62656e2874b1
Hassen-Ahmed <hassenbest23@gmail.com>
2025-04-13 23:35:18 +0100

    feat(front-end): created new animation and collusion detection for notifications

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls, UltraHDRLoader } from "three/examples/jsm/Addons.js";
 import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import { gsap } from "gsap/gsap-core";
 
 // scene, camera, renderer
 const scene = new THREE.Scene();
@@ -13,9 +14,14 @@ const pointer = new THREE.Vector2();
 
 let BP89 = {
   instance: null,
-  moveDistance: 3,
+  moveDistance: 4,
+  moveDuration: 0.5,
+  isMoving: false,
+  jumpHeight: 2,
+  jumpSpead: 5,
 };
-
+const walls = [];
+const objectsToRotateInLoop = [];
 const clickableObjects = [];
 let intersectObject = "";
 const intersectObjectNames = [
@@ -30,23 +36,19 @@ const sizes = {
   height: window.innerHeight,
 };
 
-// const camera = new THREE.PerspectiveCamera(
-//   60,
-//   sizes.width / sizes.height,
-//   0.1,
-//   1000
-// );
-
 const aspectRatio = sizes.width / sizes.height;
-const viewSize = 45;
+const viewSize = { value: 45 };
 
 const camera = new THREE.OrthographicCamera(
-  (-aspectRatio * viewSize) / 2,
-  (aspectRatio * viewSize) / 2,
-  viewSize / 2,
-  -viewSize / 2
+  (-aspectRatio * viewSize.value) / 2,
+  (aspectRatio * viewSize.value) / 2,
+  viewSize.value / 2,
+  -viewSize.value / 2
 );
-camera.position.set(40, 40, 40);
+
+camera.zoom = 1.2;
+camera.updateProjectionMatrix();
+const cameraOffset = new THREE.Vector3(40, 40, 40);
 
 const renderer = new THREE.WebGLRenderer({ canvas: myCanvas, antialias: true });
 renderer.setSize(sizes.width, sizes.height);
@@ -92,6 +94,14 @@ light.shadow.mapSize.height = 2048;
 scene.add(light);
 scene.add(light.target);
 
+const cubeBB = new THREE.Box3();
+const wallBBs = [
+  new THREE.Box3(),
+  new THREE.Box3(),
+  new THREE.Box3(),
+  new THREE.Box3(),
+];
+
 // glb loader
 const loader = new GLTFLoader();
 loader.load("/models/hassenPortfolio.glb", (glb) => {
@@ -100,7 +110,27 @@ loader.load("/models/hassenPortfolio.glb", (glb) => {
 
   models.traverse((model) => {
     if (intersectObjectNames.includes(model.name)) {
+      model.scale.set(0, 0, 0);
       clickableObjects.push(model);
+
+      // walls
+      const geometryWal = new THREE.BoxGeometry(2, 2, 2);
+      const material = new THREE.MeshBasicMaterial({
+        opacity: 0,
+        visible: false,
+      });
+
+      const wall = new THREE.Mesh(geometryWal, material);
+      wall.name = model.name;
+      wall.position.x = model.position.x;
+      wall.position.y = 1;
+      wall.position.z = model.position.z;
+      scene.add(wall);
+      walls.push(wall);
+    }
+
+    if (model.name.includes("wind-turbine-wing")) {
+      objectsToRotateInLoop.push(model);
     }
 
     if (model.isMesh) {
@@ -108,10 +138,10 @@ loader.load("/models/hassenPortfolio.glb", (glb) => {
       model.castShadow = true;
 
       if (model.name === "BP-89") {
-        const target = new THREE.Vector3();
-        model.getWorldPosition(target);
-        camera.position.set(target.x + 20, target.y + 30, target.z + 40);
-        camera.lookAt(target);
+        // const target = new THREE.Vector3();
+        // model.getWorldPosition(target);
+        // camera.position.set(target.x + 20, target.y + 30, target.z + 40);
+        // camera.lookAt(target);
       }
     }
 
@@ -126,32 +156,71 @@ function onPointerMove(event) {
   pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
+function moveBP89(targetPosition, targetRotationY) {
+  BP89.isMoving = true;
+  viewSize.value = 45;
+
+  const tl = gsap.timeline({
+    defaults: {
+      duration: BP89.moveDuration,
+      ease: "power1.inOut",
+    },
+    onComplete: () => (BP89.isMoving = false),
+  });
+
+  tl.to(BP89.instance.position, {
+    x: targetPosition.x,
+    z: targetPosition.z,
+  });
+
+  tl.to(
+    BP89.instance.position,
+    { y: BP89.instance.position.y + BP89.jumpHeight, yoyo: true, repeat: 1 },
+    0
+  );
+  tl.to(BP89.instance.rotation, { y: targetRotationY }, 0);
+  tl.timeScale(BP89.jumpSpead);
+}
+
 function onMove(event) {
-  console.log(event.key);
+  if (event.key.toLowerCase() === "=") BP89.jumpSpead = 5;
+  if (event.key.toLowerCase() === "-") BP89.jumpSpead = 2;
+
+  if (BP89.isMoving) return;
+
+  const targetPosition = new THREE.Vector3().copy(BP89.instance.position);
+  let targetRotationY = 0;
+
   switch (event.key.toLowerCase()) {
     case "arrowup":
     case "w":
-      BP89.instance.position.z -= BP89.moveDistance;
+      targetPosition.z -= BP89.moveDistance;
+      targetRotationY = 0;
       break;
 
     case "arrowdown":
     case "s":
-      BP89.instance.position.z += BP89.moveDistance;
+      targetPosition.z += BP89.moveDistance;
+      targetRotationY = Math.PI;
       break;
 
     case "arrowleft":
     case "a":
-      BP89.instance.position.x -= BP89.moveDistance;
+      targetPosition.x -= BP89.moveDistance;
+      targetRotationY = Math.PI / 2;
       break;
 
     case "arrowright":
     case "d":
-      BP89.instance.position.x += BP89.moveDistance;
+      targetPosition.x += BP89.moveDistance;
+      targetRotationY = -Math.PI / 2;
       break;
 
     default:
       break;
   }
+
+  moveBP89(targetPosition, targetRotationY);
 }
 
 window.addEventListener("resize", onResize);
@@ -160,7 +229,17 @@ window.addEventListener("click", () => {
   if (intersectObject === "pointer-about-me") {
     aboutMe.classList.toggle("about-me-hidden");
   }
+  if (intersectObject === "pointer-contact") {
+    console.log("pointer-contact");
+  }
+  if (intersectObject === "pointer-projects") {
+    console.log("pointer-projects");
+  }
+  if (intersectObject === "pointer-skills") {
+    console.log("pointer-skills");
+  }
 });
+
 aboutMeCloseBtn.addEventListener("click", () => {
   aboutMe.classList.add("about-me-hidden");
 });
@@ -172,10 +251,10 @@ function onResize() {
 
   const aspectROnRsize = window.innerWidth / window.innerHeight;
 
-  camera.left = (-aspectROnRsize * viewSize) / 2;
-  camera.right = (aspectROnRsize * viewSize) / 2;
-  camera.top = viewSize / 2;
-  camera.bottom = -viewSize / 2;
+  camera.left = (-aspectROnRsize * viewSize.value) / 2;
+  camera.right = (aspectROnRsize * viewSize.value) / 2;
+  camera.top = viewSize.value / 2;
+  camera.bottom = -viewSize.value / 2;
 
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -183,6 +262,28 @@ function onResize() {
 
 // animate
 function animate() {
+  if (BP89.instance) {
+    const targetCameraPosition = new THREE.Vector3(
+      BP89.instance.position.x + cameraOffset.x,
+      cameraOffset.y,
+      BP89.instance.position.z + cameraOffset.z
+    );
+    camera.position.copy(targetCameraPosition);
+    camera.lookAt(BP89.instance.position);
+    const staticLookAt = new THREE.Vector3(
+      BP89.instance.position.x - 10,
+      0,
+      BP89.instance.position.z - 10
+    );
+    controls.target.copy(staticLookAt);
+  }
+
+  // rotate in loop turbine wind
+  objectsToRotateInLoop.forEach((turbine) => {
+    turbine.rotation.z += 0.1;
+  });
+
+  // raycaster
   raycaster.setFromCamera(pointer, camera);
   const intersects = raycaster.intersectObjects(clickableObjects, true);
 
@@ -191,10 +292,31 @@ function animate() {
     intersectObject = intersects[0].object.parent.name;
   } else {
     document.body.style.cursor = "default";
+    clickableObjects.forEach((item) => {
+      item.scale.set(0, 0, 0);
+    });
     intersectObject = "";
   }
 
   // for (let i = 0; i < intersects.length; i++) {}
+
+  // colusion detection
+  if (BP89.instance) {
+    cubeBB.setFromObject(BP89.instance);
+
+    walls.forEach((wall, i) => {
+      wallBBs[i].setFromObject(wall);
+      wallBBs[i].expandByScalar(0.3);
+
+      if (cubeBB.intersectsBox(wallBBs[i])) {
+        clickableObjects.forEach((item) => {
+          if (wall.name === item.name) {
+            item.scale.set(2, 2, 2);
+          }
+        });
+      }
+    });
+  }
 
   controls.update();
   renderer.render(scene, camera);
